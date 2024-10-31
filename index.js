6715ea2ed34f7159cac01b0b001b9627453b2a39
Dmytro Voloshyn <dmytrovoloshyn@Mac-2.lan>
2024-10-31 12:42:46 -0600

    add server

---
+++ index.js
@@ -0,0 +1,25 @@
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+
+const PORT = 3000; // You can change the port number if needed
+
+http.createServer((req, res) => {
+    if (req.url === '/' || req.url === '/index.html') {
+        const filePath = path.join(__dirname, 'index.html');
+        fs.readFile(filePath, (err, content) => {
+            if (err) {
+                res.writeHead(500, { 'Content-Type': 'text/plain' });
+                res.end('Internal Server Error');
+            } else {
+                res.writeHead(200, { 'Content-Type': 'text/html' });
+                res.end(content);
+            }
+        });
+    } else {
+        res.writeHead(404, { 'Content-Type': 'text/plain' });
+        res.end('404 Not Found');
+    }
+}).listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
